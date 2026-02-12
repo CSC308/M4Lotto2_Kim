b9b0325ab9f9bb3e34b2d2d49b8f4ff787e752b5
Eun Mi Kim <koreaeunmi@gmail.com>
2026-02-12 14:38:32 -0500

    0212_4

--- M4Lotto_Kim/ViewController.swift
+++ M4Lotto_Kim/ViewController.swift
@@ -27,188 +27,48 @@ class ViewController: UIViewController {
          41-45:green
          Bonus: purple
          */
-        let rand1 = Int.random(in: 1...45)
-        numLabel1.text = "\(rand1)"
-        switch rand1{
-        case 1...10:
-            numLabel1.backgroundColor = UIColor.red
-            numLabel1.textColor = .white
-        case 11...20:
-            numLabel1.backgroundColor = UIColor.green
-            numLabel1.textColor = .black
-        case 21...30:
-            numLabel1.backgroundColor = UIColor.blue
-            numLabel1.textColor = .white
-        case 31...40:
-            numLabel1.backgroundColor = UIColor.yellow
-            numLabel1.textColor = .black
-        case 41...45:
-            numLabel1.backgroundColor = UIColor.green
-            numLabel1.textColor = .black
-        default:
-            break
-        }
         
-        let rand2 = Int.random(in: 1...45)
-        numLabel2.text = "\(rand2)"
-        switch rand2{
-        case 1...10:
-            numLabel2.backgroundColor = UIColor.red
-            numLabel2.textColor = .white
-        case 11...20:
-            numLabel2.backgroundColor = UIColor.green
-            numLabel2.textColor = .black
-        case 21...30:
-            numLabel2.backgroundColor = UIColor.blue
-            numLabel2.textColor = .white
-        case 31...40:
-            numLabel2.backgroundColor = UIColor.yellow
-            numLabel2.textColor = .black
-        case 41...45:
-            numLabel2.backgroundColor = UIColor.green
-            numLabel2.textColor = .black
-        default:
-            break
-        }
-        
-        let rand3 = Int.random(in: 1...45)
-        numLabel3.text = "\(rand3)"
-        switch rand3{
-        case 1...10:
-            numLabel3.backgroundColor = UIColor.red
-            numLabel3.textColor = .white
-        case 11...20:
-            numLabel3.backgroundColor = UIColor.green
-            numLabel3.textColor = .black
-        case 21...30:
-            numLabel3.backgroundColor = UIColor.blue
-            numLabel3.textColor = .white
-        case 31...40:
-            numLabel3.backgroundColor = UIColor.yellow
-            numLabel3.textColor = .black
-        case 41...45:
-            numLabel3.backgroundColor = UIColor.green
-            numLabel3.textColor = .black
-        default:
-            break
-        }
-        
-        let rand4 = Int.random(in: 1...45)
-        numLabel4.text = "\(rand4)"
-        switch rand4{
-        case 1...10:
-            numLabel4.backgroundColor = UIColor.red
-            numLabel4.textColor = .white
-        case 11...20:
-            numLabel4.backgroundColor = UIColor.green
-            numLabel4.textColor = .black
-        case 21...30:
-            numLabel4.backgroundColor = .blue
-            numLabel4.textColor = .white
-        case 31...40:
-            numLabel4.backgroundColor = UIColor.yellow
-            numLabel4.textColor = .black
-        case 41...45:
-            numLabel4.backgroundColor = UIColor.green
-            numLabel4.textColor = .black
-        default:
-            break
-        }
-        
-        let rand5 = Int.random(in: 1...45)
-        numLabel5.text = "\(rand5)"
-        switch rand1{
-        case 1...10:
-            numLabel5.backgroundColor = UIColor.red
-            numLabel5.textColor = .white
-        case 11...20:
-            numLabel5.backgroundColor = UIColor.green
-            numLabel5.textColor = .black
-        case 21...30:
-            numLabel5.backgroundColor = UIColor.blue
-            numLabel5.textColor = .white
-        case 31...40:
-            numLabel5.backgroundColor = UIColor.yellow
-            numLabel5.textColor = .black
-        case 41...45:
-            numLabel5.backgroundColor = UIColor.green
-            numLabel5.textColor = .black
-        default:
-            break
-        }
-        
-        let rand6 = Int.random(in: 1...45)
-        numLabel6.text = "\(rand1)"
-        switch rand6{
-        case 1...10:
-            numLabel6.backgroundColor = UIColor.red
-            numLabel6.textColor = .white
-        case 11...20:
-            numLabel6.backgroundColor = UIColor.green
-            numLabel6.textColor = .black
-        case 21...30:
-            numLabel6.backgroundColor = UIColor.blue
-            numLabel6.textColor = .white
-        case 31...40:
-            numLabel6.backgroundColor = UIColor.yellow
-            numLabel6.textColor = .black
-        case 41...45:
-            numLabel6.backgroundColor = UIColor.green
-            numLabel6.textColor = .black
-        default:
-            break
-        }
-        
-        let rand7 = Int.random(in: 1...45)
-        numLabel7.text = "\(rand7)"
-        numLabel7.backgroundColor = UIColor.purple
-        numLabel7.textColor = .white
     }
 
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        let labels = [numLabel1!, numLabel2!, numLabel3!, numLabel4!, numLabel5!, numLabel6!, numLabel7!]
-        
+    override func viewDidAppear(_ animated: Bool) {
+        let labels = [numLabel1!,numLabel2!,numLabel3!,numLabel4!,numLabel5!,numLabel6!,numLabel7!]
         var nums = [Int]()
         while nums.count < labels.count {
-            let rndNum = Int.random(in: 1...45)
-            if !nums.contains(rndNum){
-                nums.append(rndNum)
+            let rnd = Int.random(in: 1...45)
+            if !nums.contains(rnd) {
+                nums.append(rnd)
             }
         }
         let sortedNums = nums.sorted()
-//        nums.sorted(by: >) //decending order
         
-        for (index,label) in labels.enumerated(){
-            label.layer.cornerRadius = label.bounds.width / 2
+        for (index,label) in labels.enumerated() {
+            label.layer.cornerRadius = numLabel1.bounds.width / 2
             label.clipsToBounds = true
             
-            label.text = "\(sortedNums[index])"  //String Interpolation "\()"
+            label.text = "\(sortedNums[index])"
             
-            switch sortedNums[index]{
+            switch sortedNums[index] {
             case 1...10:
                 label.backgroundColor = UIColor.red
-                label.textColor = .white
+                label.textColor = UIColor.white
             case 11...20:
                 label.backgroundColor = UIColor.green
-                label.textColor = .black
+                label.textColor = UIColor.black
             case 21...30:
                 label.backgroundColor = UIColor.blue
-                label.textColor = .white
+                label.textColor = UIColor.white
             case 31...40:
                 label.backgroundColor = UIColor.yellow
-                label.textColor = .black
+                label.textColor = UIColor.white
             case 41...45:
-                label.backgroundColor = UIColor.green
-                label.textColor = .black
+                label.backgroundColor = UIColor.gray
+                label.textColor = UIColor.white
             default:
                 break
             }
-            numLabel7.backgroundColor = UIColor.purple
-            numLabel7.textColor = .white
         }
-        
+        numLabel7.backgroundColor = UIColor.purple
+        numLabel7.textColor = UIColor.white
     }
 
 }
